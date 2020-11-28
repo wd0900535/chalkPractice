9888dfbf1229479745c92d57fdd740ec6f484263
wd0900535 <46876763+wd0900535@users.noreply.github.com>
2020-11-28 11:27:45 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// William Denney
+// wd0900535
+// CIS-131-W01
+// 11/15/2020
+// Short Story with Chalk
+
+const { white } = require('chalk');
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.white.bgBlack('There once was a boy named Charlie. Charlie was 8 years old and had spent his entire life color blind.\n'));
+
+log(chalk.white.bgBlack('Charlie was in 3rd grade and his 9th birthday was coming up. He was excited becasue he was going to have a birthday party and all of his friends were coming over.\n'));
+
+log(chalk.white.bgBlack('Charlies big day had arrived and his best friend Jeremy brought him a Rubix cube. He was so excited because he loved puzzles and he just knew he would be able to solve it. He mixed up the cube, and no matter how hard he tried, Charlie couldnt put the Rubix cube back the right way.\n'));
+
+log(chalk.white.bgBlack('The next day at school, Charlie was very upset that he couldnt solve this puzzle. Charlies teacher Mrs. D had an idea. She gave him some glasses and said I think these may help you. She called them his Magic Glasses and said they would help Charlie see all the colors he had been missing.\n'));
+
+log(chalk.white.bgBlack('Charlie put the glasses on and his whole world changed. He looked around the room and saw the ') + chalk.red.bold('red apple on the teachers desk,') + chalk.blue.bold(' and the blue writing on the marker board.') + chalk.white.bgBlack('He was so happy he started to tear up!\n'));
+
+log(chalk.white.bgBlack('Charlie grabbed the rubix cube from his backpack and went to work.\n'));
+
+log(chalk.white.bgBlack('Charlie saw the cube as it should be for the first time! The colors were all out of order with its ') + chalk.white.bold('white, ') + chalk.hex('#FFA500').bold('orange, ') + chalk.green.bold('green, ') + chalk.yellow.bold('yellow, ') + chalk.blue.bold('blue, ') +chalk.white.bgBlack('and ') + chalk.red.bold('red') + chalk.white.bgBlack(' squares all jumbled up! He started rotating the cube furiously and got all the ') + chalk.yellow.bold('yellow squares facing the same way! Success! But there was a problem, the sides of the white squares didnt match up.\n'));
+
+log(chalk.yellow.bold('He knew he had to get the borders of the yellow to all be the same color. First he focused on getting the middle-side of each side to match, then the corners! Now the white side was complete and its borders all matched. He put the yellow side face down.\n'));
+
+log(chalk.yellow.bold('He knew he had to get the borders of the yellow to all be the same color. First he focused on getting the middle-side of each side to match, then the corners!') + chalk.hex('#FFA500').bold('First the Orange! ') + chalk.green.bold('Then the Green! ') + chalk.red.bold('Then the Red! ') + chalk.blue.bold('Finally the Blue! Now the ') + chalk.yellow.bold('yellow side was complete and its borders all matched. He put the yellow side face down.\n'));
+
+log(chalk.yellow.bold('As the cube sat, the center of each side was correct, but the center row corners were all wrong! Charlie spun the cube carefully until the bottom two rows of the ') + chalk.hex('#FFA500').bold('orange, ') + chalk.green.bold('green, ') + chalk.red.bold('red, and ') + chalk.blue.bold('blue sides lined up! Now all Charlie had to do was get the ') + chalk.white.bgBlack.bold('white top in place and he was done! He was so excited and so proud of himself. \n'));
+
+log(chalk.white.bgBlack.bold('He put the center squares of each side into place! All that was left were the corners! He had 4 different squares left, and each one was different. \n'));
+
+log(chalk.white.bgBlack.bold('The first was yellow,') + chalk.green.bold(' green,') + chalk.hex('#FFA500').bold(' and orange.'));
+log(chalk.hex('#FFA500').bold('The second was') + chalk.yellow.bold(' yellow,') + chalk.hex('#FFA500').bold(' orange,') + chalk.blue.bold(' and blue.'));
+log(chalk.blue.bold('The third was') + chalk.yellow.bold(' yellow,') + chalk.blue.bold(' blue,') + chalk.red.bold(' and red.'));
+log(chalk.red.bold('The last was') + chalk.yellow.bold(' yellow,') + chalk.red.bold(' red,') + chalk.green.bold(' and green. \n'));
+
+log(chalk.green.bold('Charlie started with no corners in the right place. Then fliped it until one was in place. Then filipped it one more time to find all four corners had fallen into place!'))
+
+log(chalk.green.bold('The cube was complete!') + chalk.white.bgBlack.bold('The White,') + chalk.hex('#FFA500').bold(' Orange,') + chalk.green.bold(' Green,') + chalk.red.bold(' Red,') + chalk.blue.bold(' Blue,') + chalk.yellow.bold(' and Yellow sides were all solid! Charlie had solved the puzzle! He was so excited and so proud and so thankful for his friend Jeremy and his Teacher Mrs. D. '));
